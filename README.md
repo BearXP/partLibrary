609c65fdda56dba4d6d5bffab393d594c6dc23b0
Mark Evans <mark.evans@LeicaBiosystems.com>
2022-12-14 12:11:43 +1100

    app.py - GUI now written in tkinter database.csv - Now has all the users included requirements.txt - Split out a dev version with my dev tools README.md - Updated with new status

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# partLibrary
+# Part Library
 
 ## Background
 I wanted to make a very simple tool to keep track of which user has borrowed which tool.  
@@ -10,13 +10,51 @@ There's a better tool incoming, but for the short term I thought I could cobble 
 ## Method:
 I'll use `logZero` to keep audit logs  
 I'll use `pandas` to manage the csv database
-    - Why csv? Unlike sqlite it's easy to edit using a notepad, and unlike toml or json it's obvious when data is missing.
-I'll use ~~`pytermgui`~~ `textual` as the front end. 
-    - I started with pytermgui, it seems like a really nice library, but it doesn't seem to play nice with Windows for me.
+   - Why csv? Unlike sqlite it's easy to edit using a notepad, and unlike toml or json it's obvious when data is missing.
 
-## To-do:
-[x] Made a way to modify the database
-[ ] Get the GUI working
-    [ ] Make a 'scan card to log in' page
-    [ ] Make a 'scan parts to borrow them, scan card to log out' page, with parts list of borrowed parts.
-    [ ] Make 'scan to log in' page show all the parts and their status
+I'll use ~~`pytermgui`~~ ~~`textual`~~ `tkinter` as the front end. 
+   - I started with pytermgui, it seems like a really nice library, but it doesn't seem to play nice with Windows for me.
+   - While textual was beautiful for Windows 10 onwards, it didn't render correctly on the Windows 7 PC.
+
+## To-do / Status:
+Given that the plan was to *cobble together something in a couple of hours*, I would say that the outstanding features are overkill for the time being. I might want to organise the email reminders though...
+
+I'll let Thomas add the rest.
+
+- [x] Made a way to modify the database
+- [X] Get the GUI working
+    - [X] Make a 'scan card to log in' page
+    - [X] Make a 'scan parts to borrow them, scan card to log out' page, with parts list of borrowed parts.
+    - [X] Make 'scan to log in' page show all the parts and their status
+- [ ] Generate history logs
+- [x] Add all the users to the database
+- [x] Add all equipment to the database
+- [ ] Generate reminder emails about overdue parts.
+    - Note: The Win7 PC can **NOT** be connected to the network, so it will probably need to be a think I run manually every morning.
+- [ ] Show return date (1 week from borrowed time).
+
+## How to build
+- Have a PC with Python 3.8 installed (See *python-3.8.10.exe* attached)
+  - Ensure the environmental variables are setup correctly
+- For the first time setup, go to the folder with this README.md in it using command prompt and run the commands:
+```bash
+python -m venv venv
+.\venv\Scripts\activate.bat
+pip install -r requirements.txt
+```
+- To build run the following commands
+```bash
+.\venv\Scripts\activate.bat
+auto-py-to-exe
+```
+  - Script Location: Select the app.py file
+  - One Directory
+  - Windows Based (hide the console)
+  - Additional Files:
+    - config.toml
+    - database.csv
+  - CONVERT .PY TO .EXE
+
+And you should be done.
+
+I'll mention that I keep considering running the python file on the PC, rather than generating an executable, but since I can't connect the PC to the network I can't `pip install` the required files.
--- app.py
+++ app.py
@@ -3,52 +3,120 @@
 # https://textual.textualize.io/guide/reactivity/#watch-methods
 
 
-from textual.app import App, ComposeResult
-from textual.containers import Horizontal, Vertical
-from textual.widgets import Input, Static, DataTable
+import tkinter as tk
+from tkinter import ttk
 from db import Database
 
-db = Database()
 
-
-class Homepage(App):
-    CSS_PATH = "app.css"
+class GUI:
     LoggedInId = ""
+    bgColour = "#292F36"
+    headerColour = "#4ECDC4"
+    header2Colour = "#FF6B6B"
+    header3Colour = "#FFE66D"
+    textColour = "#F7FFF7"
 
-    def compose(self) -> ComposeResult:
-        yield Horizontal(
-            Vertical(
-                Static("", classes="header", id="Username"),
-                Input(
-                    placeholder="Scan your **card to borrow**, or some equipment to return"
-                ),
-                DataTable(show_cursor=False),
-            )
+    def __init__(self):
+        self.db = Database()
+        self.compose()
+        self.run()
+
+    def run(self):
+        self.root.mainloop()
+
+    def compose(self):
+        self.__genRoot()
+        self.__genFrame()
+        self.__genHeader()
+        self.__genInput()
+        self.__genTable()
+
+    def __genRoot(self):
+        self.root = tk.Tk()
+        self.root.title("Equipment Library")
+        self.root.geometry("640x480")
+        self.root["bg"] = self.bgColour
+
+    def __genFrame(self):
+        self.frame = tk.Frame(self.root, background=self.bgColour)
+        self.frame.grid(pady=20, padx=20)
+        self.frame.pack()
+
+    def __genHeader(self, username: str = ""):
+        # Header
+        text = "Scan your card to log in"
+        if username:
+            text = "Logged in as " + str(username)
+        self.header = tk.Label(
+            self.frame,
+            text=text,
+            background=self.headerColour,
+            foreground=self.textColour,
+            width="100",
+            height="2",
+            font="Helvetica 14 bold",
         )
+        self.header.pack()
 
-    def _setTableRows(self, rows) -> None:
-        table = self.query_one(DataTable)
-        table.clear()
-        if not table.columns:
-            table.add_columns(*["      Equipment      ", "      Borrowed By      "])
-        table.add_rows(rows)
+    def __genInput(self):
+        # entry
+        self.sv = tk.StringVar()
+        self.entry = ttk.Entry(self.frame, width=30, textvariable=self.sv)
+        self.entry.bind("<Return>", self.processInput)
+        self.entry.pack()
 
-    def on_mount(self) -> None:
-        parts = db.getParts()
-        self._setTableRows(parts)
+    def __genTable(self):
+        ttk.Style().configure(
+            "Treeview",
+            background=self.bgColour,
+            foreground=self.textColour,
+            fieldbackground=self.bgColour,
+        )
+        # scrollbar
+        self.scroller = tk.Scrollbar(self.frame, orient="vertical")
+        self.scroller.pack(side=tk.RIGHT, fill=tk.Y)
+        self.table = ttk.Treeview(
+            self.frame, yscrollcommand=self.scroller.set, height=15
+        )
+        self.table.pack()
+        self.scroller.config(command=self.table.yview)
+        # Define our columns
+        cols = {"ID": "ID", "Borrowed_By": "Borrowed By:"}
+        self.table["columns"] = tuple(cols.keys())
+        # format our columns & headers
+        self.table.column("#0", width=0, stretch=tk.NO)
+        self.table.heading("#0", text="", anchor=tk.CENTER)
+        for ref, text in cols.items():
+            self.table.column(ref, anchor=tk.CENTER, width=300)
+            self.table.heading(ref, text=text, anchor=tk.CENTER)
+        # Add the data to the table
+        self.fillTable()
+        self.table.pack()
+
+    def fillTable(self, partsList: list = None):
+        # Clear the old values:
+        self.table.delete(*self.table.get_children())
+        if partsList == None:
+            partsList = self.db.getParts()
+        for index, part in enumerate(partsList):
+            self.table.insert(
+                parent="", index="end", iid=index, text="", values=tuple(part)
+            )
 
     def _clear(self) -> None:
-        self.query_one(Input).value = ""
-        dbItem = db.getId(self.LoggedInId)
-        loggedInString = ""
+        self.sv.set("")
+        dbItem = self.db.getId(self.LoggedInId)
+        loggedInString = "Scan your card to log in"
         if dbItem:
             loggedInString = f"Logged in as {dbItem['name']}"
-        self.query_one("#Username").update(loggedInString)
+        self.header.config(text=loggedInString)
+        # print(loggedInString)
+        self.root.update()
 
-    def on_input_submitted(self, event: Input.Submitted) -> None:
+    def processInput(self, event):
         # Lookup the ID in the database
-        id = str(event.value)
-        dbItem = db.getId(id)
+        id = str(self.sv.get().strip())
+        dbItem = self.db.getId(id)
         # If it doesn't match anything
         if not dbItem:
             self._clear()
@@ -58,25 +126,25 @@ class Homepage(App):
             # If they're already logged in as that user, log them out
             if self.LoggedInId == id:
                 self.LoggedInId = ""
-                parts = db.getParts()
+                parts = self.db.getParts()
             # Log in as the user & update the screen.
             else:
                 self.LoggedInId = id
-                parts = db.getBorrowedEquipment(id)
+                parts = self.db.getBorrowedEquipment(id)
         # If it's a piece of equipment
         else:
             # Change the equipment to whoever it logged in
-            db.changeStatus(id, self.LoggedInId)
+            self.db.changeStatus(id, self.LoggedInId)
             # Update the list of parts shown
             if self.LoggedInId:
-                parts = db.getBorrowedEquipment(self.LoggedInId)
+                parts = self.db.getBorrowedEquipment(self.LoggedInId)
             else:
-                parts = db.getParts()
-        self._setTableRows(parts)
+                parts = self.db.getParts()
+        self.fillTable(parts)
         self._clear()
-        return dbItem
+        return True
 
 
 if __name__ == "__main__":
-    app = Homepage()
+    app = GUI()
     app.run()
